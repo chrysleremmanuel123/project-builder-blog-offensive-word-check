4c8bbcb6799cc4655157f9b9193c413c2ab9dd14
chrysleremmanuel123 <chrysler.emmanuel@gmail.com>
2020-08-07 20:13:28 +0530

    day4 project

--- src/utility/CheckBlogPost.java
+++ src/utility/CheckBlogPost.java
@@ -3,25 +3,75 @@ package utility;
 import model.Blog;
 
 public class CheckBlogPost extends OffensiveWordsChecker implements OffensiveWordsInterface {
+	String[] offensiveWords = {"assbag",
+			"assbandit",
+			"assbang",
+			"assbanged",
+			"assbanger",
+			"assbangs",
+			"assbite",
+			"assclown",
+			"asscock",
+			"asscracker",
+			"asses",
+			"assface",
+			"assfaces",
+			"assfuck",
+			"assfucker",
+			"ass-fucker",
+			"assfukka",
+			"assgoblin",
+			"assh0le",
+			"asshat",
+			"ass-hat",
+			"asshead",
+			"assho1e",
+			"asshole",
+			"assholes",
+			"asshopper",
+			"ass-jabber",
+			"assjacker",
+			"asslick",
+
+			"asswipes"};
+
+	int length = offensiveWords.length;
 
 
 	public boolean checkBlogTitle(Blog blog) {
+		String title = blog.getBlogTitle();
+		for(int i=0;i<offensiveWords.length;i++) {
+
+			if(offensiveWords[i].equals(title)) {
+				System.out.println("Offensive word found");
+				return true;
+			}
+
+
+		}
 
 		return false;
 	}
 
 
-	public boolean checkBlogDescription(Blog blog) {
 
-		return false;
+	public boolean checkBlogDescription(Blog blog) {
+		for(int i=0;i<offensiveWords.length;i++) {
+			if(offensiveWords[i].equals(blog.getBlogDescription())) {
+				return false;
+			}
+
+
+		}
+		return true;
 	}
 
 	@Override
 	public boolean checkBlog(Blog blog) {
 	if(checkBlogTitle(blog)&& checkBlogDescription(blog)) 
-		return true;
-	else
 		return false;
+	else
+		return true;
 	}
 
 }
